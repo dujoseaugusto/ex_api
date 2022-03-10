a42d28c4e8fdc2cbc19ea549e9124f53457fbfe6
dujoseaugusto <45475018+dujoseaugusto@users.noreply.github.com>
2022-03-10 09:42:05 -0300

    fix

--- lib/ex_api.ex
+++ lib/ex_api.ex
@@ -7,9 +7,4 @@ defmodule ExApi do
   defdelegate update_user(params), to: User.Update, as: :call
 
   defdelegate fetch_conversion(params,paramsTwo,user), to: Conversion.Get, as: :call
-
-  ##defdelegate create_user_pokemon(params), to: TrainerPokemon.Create, as: :call
-  ##defdelegate delete_user_pokemon(params), to: TrainerPokemon.Delete, as: :call
-  ##defdelegate fetch_user_pokemon(params), to: TrainerPokemon.Get, as: :call
-  ##defdelegate update_user_pokemon(params), to: TrainerPokemon.Update, as: :call
 end
